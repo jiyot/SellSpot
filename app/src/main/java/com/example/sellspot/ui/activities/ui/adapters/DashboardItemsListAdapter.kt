afab0623d242ed4958e821009e901e268efbac9a
Ranjith-2022 <ranjithmailbox1@gmail.com>
2023-06-18 23:54:34 -0400

    Sel - 6 Product working with the detail screen, Able to move product to detail screen Now!!!

--- app/src/main/java/com/example/sellspot/ui/activities/ui/adapters/DashboardItemsListAdapter.kt
+++ app/src/main/java/com/example/sellspot/ui/activities/ui/adapters/DashboardItemsListAdapter.kt
@@ -1,6 +1,5 @@
 package com.myshoppal.ui.adapters
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -10,69 +9,43 @@ import com.example.sellspot.databinding.ItemDashboardLayoutBinding
 import com.example.sellspot.model.Product
 import com.myshoppal.utils.GlideLoader
 
-open class DashboardItemsListAdapter(
+class DashboardItemsListAdapter(
     private val context: Context,
     private var list: ArrayList<Product>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<DashboardItemsListAdapter.MyViewHolder>() {
 
-    private var _itemBinding: ItemDashboardLayoutBinding? = null
-    private val itemBinding get() = _itemBinding!!
+    private lateinit var binding: ItemDashboardLayoutBinding
     private var onClickListener: OnClickListener? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        _itemBinding = ItemDashboardLayoutBinding.inflate(LayoutInflater.from(context), parent, false)
-        return MyViewHolder(itemBinding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        binding = ItemDashboardLayoutBinding.inflate(LayoutInflater.from(context), parent, false)
+        return MyViewHolder(binding.root)
     }
 
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val model = list[position]
 
-        if (holder is MyViewHolder) {
-            GlideLoader(context).loadProductPicture(model.image, itemBinding.ivDashboardItemImage)
-            itemBinding.tvDashboardItemTitle.text = model.title
-            itemBinding.tvDashboardItemPrice.text = "$${model.price}"
+        with(binding) {
+            GlideLoader(context).loadProductPicture(model.image, ivDashboardItemImage)
+            tvDashboardItemTitle.text = model.title
+            tvDashboardItemPrice.text = "$${model.price}"
+
+            root.setOnClickListener {
+                onClickListener?.onClick(position, model)
+            }
         }
-
-    }
-
-    override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
-        if (holder is MyViewHolder) {
-            // Clear any click listeners or other references here
-        }
-        super.onViewRecycled(holder)
-    }
-
-    /**
-     * Gets the number of items in the list
-     */
-
-    // TODO Step 3: Create A function for OnClickListener where the Interface is the expected parameter and assigned to the global variable.
-    // START
-    /**
-     * A function for OnClickListener where the Interface is the expected parameter and assigned to the global variable.
-     *
-     * @param onClickListener
-     */
-    fun setOnClickListener(onClickListener: OnClickListener) {
-        this.onClickListener = onClickListener
-    }
-    // END
-
-    // TODO Step 1: Create an interface for OnClickListener.
-    /**
-     * An interface for onclick items.
-     */
-    interface OnClickListener {
-
-        // TODO Step 4: Define a function to get the required params when user clicks on the item view in the interface.
-        // START
-        fun onClick(position: Int, product: Product)
-        // END
     }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+        onClickListener = listener
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, product: Product)
     }
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
