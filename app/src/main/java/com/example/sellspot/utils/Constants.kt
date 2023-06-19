d11723367ed90f20491b8212f7add013e2da0da3
jiyot <jiyotbabrah12@gmail.com>
2023-06-19 11:53:06 -0400

    SEL-64 Product Detail Screen (Bug Fixed)

--- app/src/main/java/com/example/sellspot/utils/Constants.kt
+++ app/src/main/java/com/example/sellspot/utils/Constants.kt
@@ -20,7 +20,6 @@ object Constants {
     //A unique code for asking the Read Storage Permission using this we will be check and identify in the method onRequestPermissionsResult in the Base Activity.
     const val READ_STORAGE_PERMISSION_CODE = 2
 
-    const val EXTRA_PRODUCT_ID: String = "extra_product_id"
 
     // A unique code of image selection from Phone Storage.
     const val PICK_IMAGE_REQUEST_CODE = 2
@@ -50,6 +49,7 @@ object Constants {
     const val PRODUCT_IMAGE: String = "Product_Image"
 
     const val EXTRA_PRODUCT_ID: String = "extra_product_id"
+
 
     const val EXTRA_PRODUCT_OWNER_ID: String = "extra_product_owner_id"
 
