290a01b3dc48ee8ca39abc0f7e31f95dcafdf3db
jiyot <jiyotbabrah12@gmail.com>
2023-07-13 14:37:10 -0400

    SEL97 Share from ProductDetailsActivity

--- app/src/main/java/com/example/sellspot/ui/activities/ui/activities/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/sellspot/ui/activities/ui/activities/ProductDetailsActivity.kt
@@ -2,7 +2,10 @@ package com.example.sellspot.ui.activities.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
+import android.view.MenuItem
 import android.view.View
+import android.widget.ImageButton
 import android.widget.Toast
 import com.example.sellspot.R
 import com.example.sellspot.databinding.ActivityProductDetailsBinding
@@ -15,45 +18,36 @@ import com.myshoppal.utils.GlideLoader
 /**
  * Product Details Screen.
  */
-
 class ProductDetailsActivity : BaseActivity(), View.OnClickListener {
-
     private lateinit var mProductDetails: Product
     private var mProductOwnerId: String = ""
-
-    // A global variable for product id.
     private var mProductId: String = ""
-
     private lateinit var binding: ActivityProductDetailsBinding
 
     /**
      * This function is auto created by Android when the Activity Class is created.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        //This call the parent constructor
         super.onCreate(savedInstanceState)
         binding = ActivityProductDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         if (intent.hasExtra(Constants.EXTRA_PRODUCT_ID)) {
-            mProductId =
-                intent.getStringExtra(Constants.EXTRA_PRODUCT_ID)!!
+            mProductId = intent.getStringExtra(Constants.EXTRA_PRODUCT_ID)!!
         }
 
-        //var productOwnerId: String = ""
-
         if (intent.hasExtra(Constants.EXTRA_PRODUCT_OWNER_ID)) {
-            mProductOwnerId =
-                intent.getStringExtra(Constants.EXTRA_PRODUCT_OWNER_ID)!!
+            mProductOwnerId = intent.getStringExtra(Constants.EXTRA_PRODUCT_OWNER_ID)!!
         }
 
         setupActionBar()
+        setupShareButton()
 
         if (FirebaseClass().getCurrentUserID() == mProductOwnerId) {
             binding.btnAddToCart.visibility = View.GONE
             binding.btnGoToCart.visibility = View.GONE
         } else {
-            binding.btnAddToCart.visibility  = View.VISIBLE
+            binding.btnAddToCart.visibility = View.VISIBLE
         }
 
         binding.btnAddToCart.setOnClickListener(this)
@@ -62,26 +56,106 @@ class ProductDetailsActivity : BaseActivity(), View.OnClickListener {
         getProductDetails()
     }
 
-    override fun onClick(v: View?) {
-        if (v != null) {
-            when (v.id) {
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarProductDetailsActivity)
 
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.setTitle(R.string.title_product_details)
+
+            // Add share button
+            val shareButton = binding.toolbarProductDetailsActivity.findViewById<ImageButton>(R.id.btn_share)
+            shareButton.visibility = View.VISIBLE
+
+            shareButton.setOnClickListener {
+                shareProduct()
+            }
+        }
+
+        binding.toolbarProductDetailsActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun setupShareButton() {
+        val shareButton = binding.toolbarProductDetailsActivity.findViewById<ImageButton>(R.id.btn_share)
+        shareButton.visibility = View.VISIBLE
+        shareButton.setOnClickListener {
+            shareProduct()
+        }
+    }
+
+
+//    private fun shareProduct() {
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.type = "text/plain"
+//        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Check out this product")
+//        shareIntent.putExtra(
+//            Intent.EXTRA_TEXT,
+//            "I found this amazing product: ${mProductDetails.title}\n\n${mProductDetails.description}"
+//        )
+//        startActivity(Intent.createChooser(shareIntent, "Share via"))
+//    }
+
+
+//    private fun shareProduct() {
+//        val message = "I found this amazing product: ${mProductDetails.title}\n\n${mProductDetails.description}"
+//
+//        // Create a share intent with the message
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.type = "text/plain"
+//        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Check out this product")
+//        shareIntent.putExtra(Intent.EXTRA_TEXT, "$message\n\nOpen in SellSpot: sellspotapp://example.com?productId=$mProductId&userId=$mProductOwnerId")
+//
+//        // Create a chooser intent to give the user the option to share via the app or other apps
+//        val chooserIntent = Intent.createChooser(shareIntent, "Share via")
+//        startActivity(chooserIntent)
+//    }
+
+    private fun shareProduct() {
+        val message = "I found this amazing product: ${mProductDetails.title}\n\n${mProductDetails.description}"
+
+        // Create the deep link URL
+        val deepLinkUrl = "sellspotapp://example.com?productId=$mProductId&userId=$mProductOwnerId"
+
+        // Create a share intent with the message as HTML
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/html"
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Check out this product")
+        shareIntent.putExtra(
+            Intent.EXTRA_TEXT,
+            Html.fromHtml("$message<br><br>Open in SellSpot: \n <a href=\"$deepLinkUrl\">$deepLinkUrl</a>", Html.FROM_HTML_MODE_LEGACY)
+        )
+
+        // Create a chooser intent to give the user the option to share via the app or other apps
+        val chooserIntent = Intent.createChooser(shareIntent, "Share via")
+
+        // Check if there are any apps available to handle the share intent
+        if (shareIntent.resolveActivity(packageManager) != null) {
+            startActivity(chooserIntent)
+        } else {
+            // Fallback: Open a toast message indicating that the deep link is not supported
+            Toast.makeText(this, "App not installed or deep linking not supported", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
+    override fun onClick(v: View?) {
+        v?.let {
+            when (it.id) {
                 R.id.btn_add_to_cart -> {
                     addToCart()
                 }
-
-                R.id.btn_go_to_cart->{
+                R.id.btn_go_to_cart -> {
                     startActivity(Intent(this@ProductDetailsActivity, CartListActivity::class.java))
                 }
             }
         }
     }
 
-    /**
-     * A function to prepare the cart item to add it to the cart in cloud firestore.
-     */
     private fun addToCart() {
-
         val addToCart = Cart(
             FirebaseClass().getCurrentUserID(),
             mProductOwnerId,
@@ -92,50 +166,18 @@ class ProductDetailsActivity : BaseActivity(), View.OnClickListener {
             Constants.DEFAULT_CART_QUANTITY
         )
 
-        // Show the progress dialog
         showProgressDialog(resources.getString(R.string.please_wait))
-
         FirebaseClass().addCartItems(this@ProductDetailsActivity, addToCart)
     }
 
-    /**
-     * A function for actionBar Setup.
-     */
-    private fun setupActionBar() {
-
-        setSupportActionBar(binding.toolbarProductDetailsActivity)
-
-        val actionBar = supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
-        }
-
-        binding.toolbarProductDetailsActivity.setNavigationOnClickListener { onBackPressed() }
-    }
-
-    /**
-     * A function to call the firestore class function that will get the product details from cloud firestore based on the product id.
-     */
     private fun getProductDetails() {
-
-        // Show the product dialog
         showProgressDialog(resources.getString(R.string.please_wait))
-
-        // Call the function of FirestoreClass to get the product details.
         FirebaseClass().getProductDetails(this@ProductDetailsActivity, mProductId)
     }
 
-    /**
-     * A function to notify the success result of the product details based on the product id.
-     *
-     * @param product A model class with product details.
-     */
     fun productDetailsSuccess(product: Product) {
-
         mProductDetails = product
 
-        // Populate the product details in the UI.
         GlideLoader(this@ProductDetailsActivity).loadProductPicture(
             product.image,
             binding.ivProductDetailImage
@@ -146,45 +188,37 @@ class ProductDetailsActivity : BaseActivity(), View.OnClickListener {
         binding.tvProductDetailsDescription.text = product.description
         binding.tvProductDetailsAvailableQuantity.text = product.stock_quantity
 
-        // There is no need to check the cart list if the product owner himself is seeing the product details.
-        if (FirebaseClass().getCurrentUserID() == product.user_id) {
-            // Hide Progress dialog.
-            hideProgressDialog()
-        } else {
+        if (FirebaseClass().getCurrentUserID() != product.user_id) {
             FirebaseClass().checkIfItemExistInCart(this@ProductDetailsActivity, mProductId)
+        } else {
+            hideProgressDialog()
         }
     }
 
-    /**
-     * A function to notify the success result of item exists in the cart.
-     */
     fun productExistsInCart() {
-
-        // Hide the progress dialog.
         hideProgressDialog()
-
-        // Hide the AddToCart button if the item is already in the cart.
         binding.btnAddToCart.visibility = View.GONE
-        // Show the GoToCart button if the item is already in the cart. User can update the quantity from the cart list screen if he wants.
         binding.btnGoToCart.visibility = View.VISIBLE
     }
 
-    /**
-     * A function to notify the success result of item added to the to cart.
-     */
     fun addToCartSuccess() {
-        // Hide the progress dialog.
         hideProgressDialog()
-
         Toast.makeText(
             this@ProductDetailsActivity,
             resources.getString(R.string.success_message_item_added_to_cart),
             Toast.LENGTH_SHORT
         ).show()
-
-        // Hide the AddToCart button if the item is already in the cart.
         binding.btnAddToCart.visibility = View.GONE
-        // Show the GoToCart button if the item is already in the cart. User can update the quantity from the cart list screen if he wants.
         binding.btnGoToCart.visibility = View.VISIBLE
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
