c4bf2d1e2ea72a647b32682dfaf09205ec7ed5a5
jiyot <jiyotbabrah12@gmail.com>
2023-07-26 21:38:41 -0400

    SEL-101 Time Span added in ChatActivity.kt

--- app/src/main/java/com/example/sellspot/model/Message.kt
+++ app/src/main/java/com/example/sellspot/model/Message.kt
@@ -1,13 +1,20 @@
 package com.example.sellspot.model
 
+import com.google.firebase.database.ServerValue
+
 class Message {
     var message: String? = null
+        private set
     var senderId: String? = null
+        private set
+    var timestamp // Change the type to Object
+            : Any? = null
+        private set
 
-    constructor(){}
-
-    constructor(message: String?, senderId: String?){
+    constructor() {}
+    constructor(message: String?, senderId: String?) {
         this.message = message
         this.senderId = senderId
+        timestamp = ServerValue.TIMESTAMP // Use ServerValue.TIMESTAMP to get the server timestamp
     }
 }
